1b74250cda3c995b820aeb91e59d5211702aabf1
Felipe Silva <felipe.silva@cern.ch>
2022-05-04 16:03:33 -0400

    Does it make sense?

--- EventFilter/RPCRawToDigi/python/RPCRawToDigi_cfi.py
+++ EventFilter/RPCRawToDigi/python/RPCRawToDigi_cfi.py
@@ -1,10 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-import EventFilter.RPCRawToDigi.rpcUnpacker_cfi
-muonRPCDigis = EventFilter.RPCRawToDigi.rpcUnpacker_cfi.rpcunpacker.clone()
+# import EventFilter.RPCRawToDigi.rpcUnpacker_cfi
+# muonRPCDigis = EventFilter.RPCRawToDigi.rpcUnpacker_cfi.rpcunpacker.clone()
 
 from Configuration.Eras.Modifier_run3_RPC_cff import run3_RPC
-from EventFilter.RPCRawToDigi.rpcDigiMerger_cfi import rpcDigiMerger 
+
+from EventFilter.RPCRawToDigi.rpcDigiMerger_cff import rpcDigiMerger 
+muonRPCDigis = EventFilter.RPCRawToDigi.rpcDigiMerger_cff.rpcDigiMerger.clone()
+
 
 run3_RPC.toReplaceWith(muonRPCDigis,rpcDigiMerger)
 
